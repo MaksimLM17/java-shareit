f82e330cca7fecc9ffefc72c0b723d987328abfa
Максим <max-282@yandex.ru>
2025-07-15 22:36:06 +0500

    Final task 16th sprint (fix ItermRequestClient)

--- gateway/src/main/java/ru/practicum/shareit/request/RequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestClient.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.request;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -11,11 +12,13 @@ import ru.practicum.shareit.client.BaseClient;
 @Service
 public class RequestClient extends BaseClient {
 
+    private static final String API_PREFIX = "/requests";
+
     @Autowired
-    public RequestClient(RestTemplateBuilder builder) {
+    public RequestClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:9090/requests"))
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
                         .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
                         .build()
         );
